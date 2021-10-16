c98e016550b81262fd84e97edede34dd60775f23
bipoza <bipoza@gmail.com>
2021-10-16 12:56:55 +0200

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import Icons from "./Icons";
+function NavBar() {
+
+    return (
+        <Navbar bg="light" expand="lg" class="mb-5">
+            <Container>
+                <Navbar.Brand href="#home">Kontrata</Navbar.Brand>
+
+                <Navbar.Collapse id="basic-navbar-nav" class="f-r">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Language" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Euskara</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Español</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#home"><Icons.Github style={{ width: '32px' }} /></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
