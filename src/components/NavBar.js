2838d2b9a8155d165417777de7f66f504c5a4ef8
bipoza <bipoza@gmail.com>
2021-10-17 19:54:19 +0200

    external links (github)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Navbar as BootstrapNavbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar as BootstrapNavbar, Container, Nav } from 'react-bootstrap';
+import { EXTERNAL_LINKS } from "../constants";
 import Icon from './Icon';
 import LanguageSelector from "./LanguageSelector";
 
@@ -7,12 +8,12 @@ function Navbar() {
     return (
         <BootstrapNavbar bg="light" expand="lg" class="mb-5">
             <Container>
-                <BootstrapNavbar.Brand href="#home">Kontrata</BootstrapNavbar.Brand>
+                <BootstrapNavbar.Brand>Kontrata</BootstrapNavbar.Brand>
 
                 <BootstrapNavbar.Collapse id="basic-navbar-nav" class="f-r">
                     <Nav className="me-auto">
                         <LanguageSelector />
-                        <Nav.Link href="#home"><Icon name="github" /></Nav.Link>
+                        <Nav.Link href={EXTERNAL_LINKS.GITHUB} target="_blank"><Icon name="github" /></Nav.Link>
                     </Nav>
                 </BootstrapNavbar.Collapse>
             </Container>
--- src/constants.js
+++ src/constants.js
@@ -6,3 +6,7 @@ export const LANGUAGES = {
 export const DB_KEYS = {
     SELECTED_LANGUAGE: 'selected_language'
 }
+
+export const EXTERNAL_LINKS = {
+    GITHUB: 'https://github.com/erral'
+}
