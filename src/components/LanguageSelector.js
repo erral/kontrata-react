97b22c14d22efbc99622614e80fc80c05329cf18
bipoza <bipoza@gmail.com>
2021-10-17 19:49:39 +0200

    simplified click

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -7,15 +7,11 @@ import useLocalStorage from "../utils/useLocalStorage";
 function LanguageSelector({ icon = "language" }) {
     const [language, setLanguage] = useLocalStorage(DB_KEYS.SELECTED_LANGUAGE, Object.keys(LANGUAGES)[0]);
 
-    const selectLanguage = (lang) => {
-        console.log("lang: ", lang);
-        setLanguage(lang)
-    };
     return (
         <NavDropdown title={<><Icon name={icon} /> {language}</>} id="basic-nav-dropdown">
             {Object.entries(LANGUAGES).map(([key, value]) => {
                 return (
-                    <NavDropdown.Item onClick={() => { selectLanguage(key) }}>{value.toString()}</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => { setLanguage(key) }}>{value.toString()}</NavDropdown.Item>
                 );
             })}
         </NavDropdown>
