2f88ec0db62a58e5207137ac1ad0d940b23eb5da
bipoza <bipoza@gmail.com>
2021-10-17 19:32:55 +0200

    new language icon

---
+++ src/components/Components.md
@@ -0,0 +1,19 @@
+# Available components
+
+## Navbar
+Todo
+
+## LanguageSelector
+Todo
+
+## Icon
+Example:
+```
+import { Icon } from './components';
+
+<Icon name="github" size="64px"/>
+```
+
+Params:
+* name: "github" | "language"
+* size: default: "32px"
--- src/components/Icon/icons-gallery.js
+++ src/components/Icon/icons-gallery.js
@@ -1,8 +1,11 @@
 import { ReactComponent as GithubSVG } from './svgs/logo-github.svg';
+import { ReactComponent as LanguageSVG } from './svgs/language-outline.svg';
+
 
 export const IconsGallery = (name, size) => {
     const icons = {
-        github: <GithubSVG style={{ width: size }} />
+        github: <GithubSVG style={{ width: size }} />,
+        language: <LanguageSVG style={{ width: size }} />
     }
     return icons[name];
 }
