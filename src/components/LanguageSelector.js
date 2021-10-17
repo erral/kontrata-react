29b70d3f54e4166c06f8a0c23646f49474f11e0e
bipoza <bipoza@gmail.com>
2021-10-17 20:12:40 +0200

    optional LanguageSelector icon

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -4,11 +4,11 @@ import Icon from './Icon';
 import { DB_KEYS, LANGUAGES } from '../constants';
 import useLocalStorage from "../utils/useLocalStorage";
 
-function LanguageSelector({ icon = "language" }) {
+function LanguageSelector({ icon }) {
     const [language, setLanguage] = useLocalStorage(DB_KEYS.SELECTED_LANGUAGE, Object.keys(LANGUAGES)[0]);
 
     return (
-        <NavDropdown title={<><Icon name={icon} /> {language}</>} id="basic-nav-dropdown">
+        <NavDropdown title={<>{icon && <Icon name={icon} />} {language}</>} id="basic-nav-dropdown">
             {Object.entries(LANGUAGES).map(([key, value]) => {
                 return (
                     <NavDropdown.Item onClick={() => { setLanguage(key) }}>{value.toString()}</NavDropdown.Item>
