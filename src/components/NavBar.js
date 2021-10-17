b3657368ff59795c997a49b04a64c47c6988260d
bipoza <bipoza@gmail.com>
2021-10-17 19:06:17 +0200

    renamed bootstrap navbar to create our own navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,25 @@
 import React from "react"
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import Icon from "./Icon";
-function NavBar() {
+import { Navbar as BootstrapNavbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import Icon from './Icon';
 
+function Navbar() {
     return (
-        <Navbar bg="light" expand="lg" class="mb-5">
+        <BootstrapNavbar bg="light" expand="lg" class="mb-5">
             <Container>
-                <Navbar.Brand href="#home">Kontrata</Navbar.Brand>
+                <BootstrapNavbar.Brand href="#home">Kontrata</BootstrapNavbar.Brand>
 
-                <Navbar.Collapse id="basic-navbar-nav" class="f-r">
+                <BootstrapNavbar.Collapse id="basic-navbar-nav" class="f-r">
                     <Nav className="me-auto">
                         <NavDropdown title="Language" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Euskara</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Español</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#home"><Icon name="github"/></Nav.Link>
+                        <Nav.Link href="#home"><Icon name="github" /></Nav.Link>
                     </Nav>
-                </Navbar.Collapse>
+                </BootstrapNavbar.Collapse>
             </Container>
-        </Navbar>
+        </BootstrapNavbar>
     );
 }
 
-export default NavBar;
+export default Navbar;
