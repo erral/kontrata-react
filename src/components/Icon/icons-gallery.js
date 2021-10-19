0f4b33f42769b15bafa491c729f3c37dd246f6a2
bipoza <bipoza@gmail.com>
2021-10-19 21:55:04 +0200

    added new icons to Icon component

--- src/components/Icon/icons-gallery.js
+++ src/components/Icon/icons-gallery.js
@@ -1,11 +1,15 @@
 import { ReactComponent as GithubSVG } from './svgs/logo-github.svg';
 import { ReactComponent as LanguageSVG } from './svgs/language-outline.svg';
+import { ReactComponent as MoonSVG } from './svgs/moon-outline.svg';
+import { ReactComponent as SunSVG } from './svgs/sunny-outline.svg';
 
-
-export const IconsGallery = (name, size) => {
+export const IconsGallery = (name, size, color) => {
+    const icon_design = { width: size, color: color }
     const icons = {
-        github: <GithubSVG style={{ width: size }} />,
-        language: <LanguageSVG style={{ width: size }} />
+        github: <GithubSVG style={icon_design} />,
+        language: <LanguageSVG style={icon_design} />,
+        moon: <MoonSVG style={icon_design} />,
+        sun: <SunSVG style={icon_design} />
     }
     return icons[name];
 }
