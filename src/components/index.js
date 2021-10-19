d7cfa73a85e22d4ba6bf585dcc443805afdd28cb
bipoza <bipoza@gmail.com>
2021-10-19 21:55:56 +0200

    basic design

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from './NavBar';
 import Icon from './Icon';
 import LanguageSelector from './LanguageSelector';
 import Footer from './Footer';
+import DarkModeToggle from './DarkModeToggle';
 
-export { Navbar, Icon, LanguageSelector, Footer };
+export { Navbar, Icon, LanguageSelector, Footer, DarkModeToggle };
