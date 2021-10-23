495bbdef105aa5cd836b8775d08fc9a372bd3795
bipoza <bipoza@gmail.com>
2021-10-23 13:02:05 +0200

    reorganized components

--- src/App.js
+++ src/App.js
@@ -1,164 +1,53 @@
-import React, { Component } from 'react';
-import { ReactiveBase, DataSearch, SingleList, ReactiveList, RangeSlider, MultiRange, SelectedFilters } from "@appbaseio/reactivesearch";
-import { Row, Col, Container, Card, Button, Modal, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Icon, LanguageSelector, Footer, DarkModeToggle } from './components';
-import { EXTERNAL_LINKS } from "./constants";
-import './style.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+import { IntlProvider } from "react-intl";
+import messages_eu from "./translations/eu.json";
+import messages_es from "./translations/es.json";
+import routes from './routes';
+import useLocalStorage from './utils/useLocalStorage';
+import { DB_KEYS, EXTERNAL_LINKS } from './constants';
+import { Navbar, Icon, LanguageSelector, DarkModeToggle } from './components';
+import { FormattedMessage } from 'react-intl';
+import { Nav } from 'react-bootstrap';
+
+const messages = {
+  'eu': messages_eu,
+  'es': messages_es
+};
+const default_language = navigator.language.split(/[-_]/)[0];  // language without region code
 
 function App() {
-
-  const [modalContent, setModalContent] = React.useState(null);
-
-  const handleClose = () => setModalContent(null);
-  const handleShow = (content) => setModalContent(content);
-
-  const numberFormat = (value) =>
-    new Intl.NumberFormat('es-ES', {
-      style: 'currency',
-      currency: 'EUR'
-    }).format(value);
+  const [language, setLanguage] = useLocalStorage(DB_KEYS.SELECTED_LANGUAGE, default_language);
 
   return (
     <>
-      <Navbar>
-        <LanguageSelector />
-        <Nav.Link href={EXTERNAL_LINKS.GITHUB} target="_blank"><Icon name="github" size="28px" /></Nav.Link>
-        <DarkModeToggle />
-      </Navbar>
-      <ReactiveBase
-        url="http://localhost:9200"
-        app="contracts_es"
-      >
-        <Container>
-          <Row>
-            <Col md={4}>
-              <SingleList
-                componentId="Authority"
-                dataField="authority.keyword"
-                title="Authority"
-                sortBy="asc"
-                size={10000}
-              />
-
-              <SingleList
-                componentId="Status"
-                dataField="status.keyword"
-                title="Status"
-                sortBy="asc"
-              />
-
-              <SingleList
-                componentId="Type"
-                dataField="contract_type.keyword"
-                title="Contract type"
-                sortBy="asc"
-              />
-
-              <RangeSlider
-                componentId="PriceSensor"
-                dataField="resolution_0.priceWithVAT"
-                title="Price"
-                range={{
-                  start: 0,
-                  end: 1000000
-                }}
-                rangeLabels={{
-                  start: '0',
-                  end: '1000000'
-                }}
-                showHistogram={true}
-                snap={false}
-              />
-
-            </Col>
-            <Col md={8}>
-              <DataSearch
-                componentId="SearchSensor"
-                dataField={["title", "offerers.name", "winner_0.name"]}
-                autosuggest={false}
-              />
-
-              <SelectedFilters />
-
-              <ReactiveList
-                componentId="SearchResult"
-                pagination={true}
-                paginationAt="both"
-                react={{
-                  "and": [
-                    "Authority",
-                    "Status",
-                    "Type",
-                    "SearchSensor", "PriceSensor"]
-                }}
-                sortOptions={[
-                  {
-                    label: "Price (high to low)",
-                    dataField: "resolution_0.priceWithVAT",
-                    sortBy: "desc"
-                  },
-                  {
-                    label: "Price (low to high)",
-                    dataField: "resolution_0.priceWithVAT",
-                    sortBy: "asc"
-                  }
-              ]}
-              renderItem={(res) =>
-                <>
-                  <Card className="mb-2">
-                    <Card.Body>
-                      <Card.Title>{res.title}</Card.Title>
-                      <Card.Text>
-                        ID: {res.id}<br />
-                        Authority: {res.authority}<br />
-                        Status: {res.status}<br />
-                        Winner: {res.winner_0?.name}<br />
-                        Price: {numberFormat(res.resolution_0?.priceWithVAT)}<br />
-                        Offerers: {res.offerers.map((item) => item.name + ', ')} <br />
-                        <Button variant="primary" onClick={() => handleShow(res)}>See more details</Button>
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                 <Modal show={!!modalContent} onHide={handleClose} fullscreen={true}>
-                 {modalContent && 
-                    <>
-                      <Modal.Header closeButton>
-                        <Modal.Title>{modalContent.title}</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body><pre>{JSON.stringify(modalContent, null, 2)}</pre></Modal.Body>
-                      <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                          Close
-                        </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                          Save Changes
-                        </Button>
-                      </Modal.Footer>
-                    </>
-                    }
-                  </Modal>
-                </>
-              }
-              renderResultStats={
-                  function(stats){
-                      return (
-                          `Showing ${stats.displayedResults} of total ${stats.numberOfResults} in ${stats.time} ms`
-                      )
-                  }
-                }
-              />
-
-
-            </Col>
-
-          </Row>
-        </Container>
-      </ReactiveBase>
-      <Footer>
-        <p>Kontrata</p>
-        <a href=""></a>
-        </Footer>
+      <IntlProvider locale={language} messages={messages?.[language]}>
+        <BrowserRouter>
+          <Route exact path="/">
+            <Redirect to={"/" + language} />
+          </Route>
+          <Switch>
+            {routes.map(route => {
+              return (
+                <Route path={route.path}>
+                  <Navbar>
+                    <FormattedMessage id="app.testing_text"
+                      defaultMessage="Edit <code>src/App.js</code> and save to reload.<br/>Now with {what}!"
+                      description="Welcome header on app main page"
+                      values={{ what: 'react-intl' }} />
+                    <LanguageSelector route={route} language={language} setLanguage={setLanguage} />
+                    <Nav.Link href={EXTERNAL_LINKS.GITHUB} target="_blank"><Icon name="github" size="28px" /></Nav.Link>
+                    <DarkModeToggle />
+                  </Navbar>
+                  <route.component />
+                </Route>
+              )
+            })}
+          </Switch>
+        </BrowserRouter>
+      </IntlProvider>
     </>
   );
 }
