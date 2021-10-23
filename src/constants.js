02bf442e02da6e4e8a26fa11d26a014080cfe7ea
ionlizarazu <ilizarazu@codesyntax.com>
2021-10-23 13:20:04 +0200

    move vars to  constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,27 @@
-export const LANGUAGES = {
-    "eu": "Euskara",
-    "es": "Español"
-}
+const LANGUAGES = {
+  eu: "Euskara",
+  es: "Español",
+};
 
-export const DB_KEYS = {
-    SELECTED_LANGUAGE: 'selected_language',
-    THEME: 'theme'
-}
+const DB_KEYS = {
+  SELECTED_LANGUAGE: "selected_language",
+  THEME: "theme",
+};
 
-export const EXTERNAL_LINKS = {
-    GITHUB: 'https://github.com/erral'
-}
+const EXTERNAL_LINKS = {
+  GITHUB: "https://github.com/erral",
+};
+
+const REACT_APP_ELASTIC_SCHEME = process.env.REACT_APP_ELASTIC_SCHEME || "http";
+const REACT_APP_ELASTIC_HOST =
+  process.env.REACT_APP_ELASTIC_HOST || "localhost";
+const REACT_APP_ELASTIC_PORT = process.env.REACT_APP_ELASTIC_PORT || "9200";
+
+export {
+  LANGUAGES,
+  DB_KEYS,
+  EXTERNAL_LINKS,
+  REACT_APP_ELASTIC_SCHEME,
+  REACT_APP_ELASTIC_HOST,
+  REACT_APP_ELASTIC_PORT,
+};
