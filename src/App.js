5b5a1f8bdb3b0b0a846b241d4ecf2a58c1bee1db
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-10-23 12:32:10 +0200

    use env vars to set ELASTIC host information

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ function App() {
       currency: 'EUR'
     }).format(value);
 
+  const REACT_APP_ELASTIC_SCHEME = process.env.REACT_APP_ELASTIC_SCHEME || "http";
+  const REACT_APP_ELASTIC_HOST = process.env.REACT_APP_ELASTIC_HOST || "localhost";
+  const REACT_APP_ELASTIC_PORT = process.env.REACT_APP_ELASTIC_PORT || "9200";
+
   return (
     <>
       <Navbar>
@@ -27,7 +31,7 @@ function App() {
         <DarkModeToggle />
       </Navbar>
       <ReactiveBase
-        url="http://localhost:9200"
+        url={`${REACT_APP_ELASTIC_SCHEME}://${REACT_APP_ELASTIC_HOST}:${REACT_APP_ELASTIC_PORT}`}
         app="contracts_es"
       >
         <Container>
