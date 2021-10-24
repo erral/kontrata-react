4b5fdc6305ffad1c813aebd224c898ba5daab1ad
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-10-24 10:35:42 +0200

    company route and links to it

--- src/components/Icon/icons-gallery.js
+++ src/components/Icon/icons-gallery.js
@@ -2,14 +2,19 @@ import { ReactComponent as GithubSVG } from './svgs/logo-github.svg';
 import { ReactComponent as LanguageSVG } from './svgs/language-outline.svg';
 import { ReactComponent as MoonSVG } from './svgs/moon-outline.svg';
 import { ReactComponent as SunSVG } from './svgs/sunny-outline.svg';
+import { ReactComponent as OkSVG } from './svgs/ok_icon.svg';
+import { ReactComponent as NotOKSVG } from './svgs/not_ok_icon.svg';
 
 export const IconsGallery = (name, size, color) => {
     const icon_design = { width: size, color: color }
     const icons = {
-        github: <GithubSVG style={icon_design} />,
-        language: <LanguageSVG style={icon_design} />,
-        moon: <MoonSVG style={icon_design} />,
-        sun: <SunSVG style={icon_design} />
+      github: <GithubSVG style={icon_design} />,
+      language: <LanguageSVG style={icon_design} />,
+      moon: <MoonSVG style={icon_design} />,
+      sun: <SunSVG style={icon_design} />,
+      ok: <OkSVG style={icon_design} />,
+      notok: <NotOKSVG style={icon_design} />,
+
     }
     return icons[name];
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { LANGUAGES } from "./constants";
 import HomePage from "./pages/HomePage";
+import CompanyPage from "./pages/CompanyPage";
+import { Redirect } from 'react-router-dom';
 
 const language_independent_routes = Object.keys(LANGUAGES).map(lang => {
   return {
@@ -18,7 +20,28 @@ const language_dependant_routes = [
     component: () => <p>AYUDA</p>,
     path:`/es/ayuda`,
     sibling:`/eu/laguntza`,
-  }
+  },
+  {
+    component: () => <CompanyPage />,
+    path:`/es/empresa/:cif`,
+    sibling:`/eu/enpresa/:cif`,
+  },
+  {
+    component: () => <CompanyPage />,
+    path:`/eu/enpresa/:cif`,
+    sibling:`/es/empresa/:cif`,
+  },
+  {
+    component: () => <Redirect to="/" />,
+    path:`/es/empresa`,
+    sibling:`/eu/enpresa`,
+  },
+  {
+    component: () => <Redirect to="/" />,
+    path:`/eu/enpresa`,
+    sibling:`/es/empresa`,
+  },
+
 ]
 console.log('language_independent_routes: ', ...language_independent_routes);
 console.log('language_dependant_routes: ', ...language_dependant_routes);
