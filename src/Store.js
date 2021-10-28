edbd50c51aa17a812af2135457c935e344236f5b
ionlizarazu <ilizarazu@codesyntax.com>
2021-10-28 18:37:26 +0200

    add Store and Reducer

---
+++ src/Store.js
@@ -0,0 +1,16 @@
+import React, { createContext, useReducer } from "react";
+import Reducer from "./Reducer";
+
+const initialState = {
+  language: "eu",
+};
+
+const Store = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, initialState);
+  return (
+    <Context.Provider value={[state, dispatch]}>{children}</Context.Provider>
+  );
+};
+
+export const Context = createContext(initialState);
+export default Store;
